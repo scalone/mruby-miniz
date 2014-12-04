a5901cc2ca38808f10ff9e76ad608ae755c9cbe7
Thiago Scalone <thiago@scalone.com.br>
2014-12-04 10:39:36 -0200

    Path to append on directory to unzip files.

--- mrblib/miniz.rb
+++ mrblib/miniz.rb
@@ -1,6 +1,6 @@
 class Miniz
-  def self.unzip(filezip)
-    dir = "#{filezip.split(".")[0]}"
+  def self.unzip(filezip, path = ".")
+    dir = "#{path}/#{filezip.split(".")[0]}"
     clean(dir)
     self._unzip(filezip, dir)
   end
