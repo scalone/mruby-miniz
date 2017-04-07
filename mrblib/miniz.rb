3e49c00f97bb1c1d97b1238a98017a92c1e03e00
Thiago Scalone <thiago@scalone.com.br>
2017-04-07 18:38:33 -0300

    Create dir to unzip if it not exists.

--- mrblib/miniz.rb
+++ mrblib/miniz.rb
@@ -1,6 +1,8 @@
 class Miniz
   def self.unzip(filezip, path = ".")
-    dir = "#{path}/#{filezip.split(".")[0]}"
+    dir_name = filezip.split(".")[-2].split("/").last
+    dir = "#{path}/#{dir_name}"
+    Dir.mkdir(dir) unless File.exists?(dir)
     self._unzip(filezip, dir)
   end
 end
