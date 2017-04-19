1e839c5810b808937eef39f7b5f981108b2f5af8
Thiago Scalone <thiago@scalone.com.br>
2017-04-19 00:28:27 -0300

    Revert "Create dir to unzip if it not exists."
    This reverts commit 3e49c00f97bb1c1d97b1238a98017a92c1e03e00.

--- mrblib/miniz.rb
+++ mrblib/miniz.rb
@@ -1,8 +1,6 @@
 class Miniz
   def self.unzip(filezip, path = ".")
-    dir_name = filezip.split(".")[-2].split("/").last
-    dir = "#{path}/#{dir_name}"
-    Dir.mkdir(dir) unless File.exists?(dir)
+    dir = "#{path}/#{filezip.split(".")[0]}"
     self._unzip(filezip, dir)
   end
 end
